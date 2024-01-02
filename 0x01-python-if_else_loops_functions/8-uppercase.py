0cd06cde7343108affd391fbbfee0bd3927e1f51
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-02 18:01:48 +0200

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -9,11 +9,8 @@ def uppercase(str):
     Returns:
         None
     """
-
     for s in str:
         if ord(s) in range(97, 123):
             s = chr(ord(s) - 32)
-
         print("{}".format(s), end="")
-
     print("")
