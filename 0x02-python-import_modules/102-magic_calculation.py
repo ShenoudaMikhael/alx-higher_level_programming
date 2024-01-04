e1b087af5bf6f44ea2703b2cc372f284c0f0bb73
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-04 21:39:35 +0200

    8. ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import dis
-
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
@@ -15,4 +12,3 @@ def magic_calculation(a, b):
         return sub(a, b)
 
 
-dis.dis(magic_calculation)
