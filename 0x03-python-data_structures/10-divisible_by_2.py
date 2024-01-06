99158f78f1dea7b66eb2db8852da6946cc5976a1
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-06 07:06:25 +0200

    10. Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,4 +3,4 @@ def divisible_by_2(my_list=[]):
     """Return a new list with True or False, depending on whether
     the integer at the same position in the original list is a multiple of 2
     The new list should have the same size as the original list"""
-    return [True if i % 2 == 0 else False for i in range(len(my_list))]
+    return [my_list[i] % 2 == 0 for i in range(len(my_list))]
