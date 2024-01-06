2dca06739b2d42cfa03451d380b7216a730f0a5b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-06 06:53:12 +0200

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python3
-
-
+#!/usr/bin/python3
 def no_c(my_string):
-    """Removes c from string"""
-    if my_string:
-        new_string = ""
-        for i in my_string:
-            if i != "c" and i != "C":
-                new_string += i
-        return new_string
+    new_string = ""
+    for i in my_string:
+        new_string += i if i != "c" and i != "C" else ""
+    return new_string
