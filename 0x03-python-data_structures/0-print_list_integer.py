78adfe28d9191a3d18cf9a4adcad33ffc2b93168
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-06 06:36:22 +0200

    0. Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,4 +5,4 @@ def print_list_integer(my_list=[]):
     """print integer list"""
 
     for i in range(len(my_list)):
-        print("{0}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
