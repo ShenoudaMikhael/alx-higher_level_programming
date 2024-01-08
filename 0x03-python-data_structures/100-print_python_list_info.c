fc4693dec035ebc52e7e85daef550def39de4237
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-08 19:10:39 +0200

    14. CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,12 +1,12 @@
 #include "python3.11/Python.h"
+/**
+ * print_python_list_info - print oython list info
+ * @p: list
+ * Return: void
+ */
 void print_python_list_info(PyObject *p)
 {
+	Py_ssize_t n = PyList_Size(p);
 
-    Py_ssize_t n = PyList_Size(p);
-    
-
-    printf("[*] Size of the Python List = %ld\n", n);
-
-
-    return;
+	printf("[*] Size of the Python List = %ld\n", n);
 }
