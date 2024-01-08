086c2ac4479144fb69b18b69aef3a96a550d19a3
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-08 20:06:05 +0200

    14. CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,7 +6,13 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t n = PyList_Size(p);
+	Py_ssize_t i;
 
-	printf("[*] Size of the Python List = %ld\n", n);
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < PyList_Size(p); i++)
+	{
+		 
+		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
 }
