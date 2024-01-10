73f82db6c562fc3e0bf2fad712e76c76067c5323
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-10 15:58:59 +0200

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!usr/bin/python3
 def square_matrix_simple(matrix=[]):
     """return square of matrix"""
-    if len(matrix) > 0:
-        new_matrix = [[row[i] ** 2 for i in range(3)] for row in matrix]
-        return new_matrix
+    new_matrix = [[row[i] ** 2 for i in range(3)] for row in matrix]
+    return new_matrix
