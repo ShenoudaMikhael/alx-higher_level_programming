1646bd8835a7f3eb297b39687e4ce2b5ae2b89af
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-10 19:24:37 +0200

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
     num = 0
     for i, l in enumerate(roman_string):
         if roman_string[i] == "I":
