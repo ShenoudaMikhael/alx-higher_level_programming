06f2a4e0b5b381d690f9173d7cf1a99e2a91a88e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-10 22:26:51 +0200

    fix1

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,6 @@
 
-#include <python3.11/Python.h>
-/*#include <Python.h>*/
+/* #include <python3.11/Python.h>*/
+#include <Python.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
