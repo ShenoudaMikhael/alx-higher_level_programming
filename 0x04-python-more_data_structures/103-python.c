07072bd0715196380c28c1ef116ddd76622aa35e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-10 22:35:10 +0200

    fix3 python bytes

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,6 @@
 
-/* #include <python3.11/Python.h>*/
-#include <Python.h>
+#include <python3.11/Python.h>
+/*#include <Python.h>*/
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
@@ -44,6 +44,26 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	(void)p;
-	return;
+	char i, size = 10;
+	PyBytesObject *bytes = (PyBytesObject *)p;
+
+	printf("[.] bytes object info\n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
+	printf("  first %d bytes: ", size);
+	for (i = 0; i < size; i++)
+	{
+		printf("%02hhx", bytes->ob_sval[i]);
+		if (i == (size - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
 }
