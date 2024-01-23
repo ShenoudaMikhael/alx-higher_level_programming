d3025e2a82d8bd1444a54e3f8d821952701399c8
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-23 20:45:16 +0200

    9. Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""square module docs"""
+
+
+class Square:
+    """Empty Sqaure Class"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, Square):
+            return self.size == __value.size
