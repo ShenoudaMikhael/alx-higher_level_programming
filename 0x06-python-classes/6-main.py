d077ac73b1c90b1195cf582cdbcbbd5d697893f2
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-23 18:52:39 +0200

    6. Coordinates of a square

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 Square = __import__("6-square").Square
 
-my_square_1 = Square(3)
+my_square_1 = Square(0)
 my_square_1.my_print()
 
 print("--")
@@ -11,7 +11,7 @@ my_square_2.my_print()
 
 print("--")
 
-my_square_3 = Square(3, (3, 0))
+my_square_3 = Square(3, (3, 2))
 my_square_3.my_print()
 
 print("--")
