caf342fc6cd55f10b6a1ffd21f517615806c5184
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-24 00:30:34 +0200

    9. Compare 2 squares

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""singly list module node """
+
+
+class Node:
+    """singly list class node"""
+
+    def __init__(self, data, node=None) -> None:
+        self.__data = data
+        self.__node = node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__node
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__node = value
+
+
+class SinglyLinkedList:
+    """Singly linked list class"""
+
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        n = Node(value)
+        if self.__head is None:
+            n.next_node = None
+
+            self.__head = n
+
+        elif self.__head.data > value:
+            n.next_node = self.__head
+            self.__head = n
+
+        else:
+            tmp = self.__head
+            while tmp.next_node is not None:
+                if tmp.next_node.data < value:
+                    tmp = tmp.next_node
+                else:
+                    break
+            n.next_node = tmp.next_node
+            tmp.next_node = n
+
+    def __str__(self):
+        tmp = self.__head
+        while tmp.next_node is not None:
+            print(
+                "{}".format(tmp.data),
+                end="\n" if tmp.next_node.next_node is not None else "",
+            )
+            tmp = tmp.next_node
+        return ""
