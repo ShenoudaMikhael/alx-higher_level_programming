08c2950fc5c64ef45683357f96aed3e94bdbbb7d
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-23 16:50:00 +0200

    4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,3 +14,11 @@ class Square:
 
     def area(self):
         return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
