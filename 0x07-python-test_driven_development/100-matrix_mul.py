0e97cafc4c59d5c2dd1f70d90d2ac9a6c469ad0b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-29 17:17:11 +0200

    matrix

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+def matrix_mul(m_a, m_b):
+    """
+    Multiplies two matrices.
+
+    Args:
+        m_a (list of lists): The first matrix to multiply.
+        m_b (list of lists): The second matrix to multiply with the first.
+    """
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all([isinstance(x, list) for x in m_a]):
+        raise TypeError("m_a must be a list of lists")
+    if not all([isinstance(x, list) for x in m_b]):
+        raise TypeError("m_b must be a list of lists")
+    if not all(
+        [
+            isinstance(x, int)
+            or isinstance(x, float)
+            for row in m_a for x in row
+            ]
+    ):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all(
+        [
+            isinstance(x, int)
+            or isinstance(x, float)
+            for row in m_b for x in row
+            ]
+    ):
+        raise TypeError("m_b should contain only integers or floats")
+
+    for _,row in enumerate(m_a):
+        if len(m_a[0]) != len(row):
+            raise TypeError("each row of m_a must be of the same size")
+
+    for _,row in enumerate(m_b):
+        if len(m_b[0]) != len(row):
+            raise TypeError("each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = []
+    rows_a = len(m_a)
+    cols_a = len(m_a[0])
+    cols_b = len(m_b[0])
+
+    for i in range(rows_a):
+        row_result = []
+        for j in range(cols_b):
+            element_sum = 0
+            for k in range(cols_a):
+                element_sum += m_a[i][k] * m_b[k][j]
+            row_result.append(element_sum)
+        result.append(row_result)
+
+    return result
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+Unittest for max_integer([..])
 """
 import unittest
 
@@ -7,10 +8,20 @@ max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """
+    Unit tests for the max_integer function
+    """
+
     def test_max_integer(self):
+        """
+        Tests the function max_integer() with different inputs.
+        """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_none_list(self):
+        """
+        If none list is passed to the function it should return None.
+        """
         self.assertEqual(max_integer(), None)
 
 
