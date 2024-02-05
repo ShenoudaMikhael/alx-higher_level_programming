3ebbc931e8dd0f4b8254bdefa0a2002a2dd19f5f
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-05 17:47:10 +0200

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,8 +5,10 @@
 class MyList(list):
     """My list class"""
 
-    def print_sorted(self) -> str:
+    def print_sorted(self) -> None:
         """print the list sorted"""
+        if not all(isinstance(x, int) for x in self):
+            raise TypeError("all items must be integers")
         a = self.copy()
         a.sort()
         return print(a)
