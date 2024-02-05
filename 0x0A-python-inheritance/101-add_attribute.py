4eefb40f761e028bd34b618d1cc6c4f16aafbcdd
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-05 21:17:52 +0200

    13. Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" add attribute module with one function"""
+"""add attribute module with one function"""
 
 
 def add_attribute(obj, name, value):
@@ -14,5 +14,4 @@ def add_attribute(obj, name, value):
     """
     if hasattr(obj, "__dict__") == False:
         raise TypeError("can't add new attribute")
-
     setattr(obj, name, value)
