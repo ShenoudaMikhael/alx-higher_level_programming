0a918cca35ffecfcb8b187902c5d936e374edc03
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-05 21:24:06 +0200

    13. Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(obj, name, value):
     """adds a new attribute to an object"""
-    if hasattr(obj, "__dict__") == False:
+    if "__dict__" not in dir(obj):
         raise TypeError("can't add new attribute")
 
     setattr(obj, name, value)
