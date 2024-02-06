100e788c9ad50e765656c93eb2cf99db7029cdf7
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 12:47:21 +0200

    fix doc

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,13 +5,6 @@
 def write_file(filename="", text=""):
     """
     Writes the contents of a file.
-
-    Args:
-        filename (str, optional): The name of the file to write. Defaults to "".
-        text (str, optional): The text to write to the file. Defaults to "".
-
-    Returns:
-        int: The number of characters written to the file.
     """
     i = 0
     with open(filename, "w", encoding="utf-8") as f:
