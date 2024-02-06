be1b321d5c51bebb75858df3d52880146e176afd
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 13:43:12 +0200

    9. Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Studen Class Module"""
+
+
 class Student:
+    """Student Class"""
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
