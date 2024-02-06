89bedb18cc37548f3f89293b07579687ea3c38c1
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 14:52:00 +0200

    10. Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -8,10 +8,6 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """
         Init function
-        Args:
-            first_name (str): Student's first name
-            last_name (str): Student's last name
-            age (int): Student's age
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -20,11 +16,7 @@ class Student:
     def to_json(self, attrs=None):
         """
         Return a JSON representation of the object
-        Args:
-            attrs (list): A list of attributes to include in the JSON representation.
-            Defaults to all attributes if not provided
         """
-
         if (
             isinstance(attrs, list) and
             all(isinstance(att, str) for att in attrs)):
