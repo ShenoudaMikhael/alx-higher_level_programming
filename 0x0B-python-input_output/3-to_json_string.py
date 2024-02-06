e0caacf64522691986d33b28b7a8515d2a51e382
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 07:43:37 +0200

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" To JSON Module """
+import json
+
+
+def to_json_string(obj):
+    """
+    This function converts the given object to a JSON string.
+
+    Args:
+        obj (object): The object to convert to a JSON string.
+
+    Returns:
+        str: The JSON string representation of the given object.
+
+    """
+    return json.dumps(obj)
