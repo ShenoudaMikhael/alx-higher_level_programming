849fb8240752f2cddc8854b2b8083c1b306ea4c1
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-07 00:19:30 +0200

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Pascal Triangle Module"""
 
-def pascal_triangle(n):
+def pascal_triangle(num):
     """pascal trinagle"""
 
     list1 = []
-    for i in range(n):
+    for i in range(num):
         list1.append([])
         list1[i].append(1)
         for j in range(1, i):
             list1[i].append(list1[i - 1][j - 1] + list1[i - 1][j])
-        if n != 0 and i > 0:
+        if num != 0 and i > 0:
             list1[i].append(1)
 
     return list1
