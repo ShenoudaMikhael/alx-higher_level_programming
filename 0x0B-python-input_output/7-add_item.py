9543bdd42fd08e56016ccf9676696c9aacc04719
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 13:03:04 +0200

    fix 7.1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 import sys
 import os
 
+
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-
 
 if __name__ == "__main__":
     a = sys.argv[1:]
