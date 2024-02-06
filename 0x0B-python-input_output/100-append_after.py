e18e7712944747cea6805fd5440bd0305ad4d812
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-07 01:30:11 +0200

    13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,9 +9,9 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
-        for i, line in enumerate(lines):
+        for index, line in enumerate(lines):
             if search_string in line:
-                lines.insert(i + 1, new_string)
-                i += 1
+                lines.insert(index + 1, new_string)
+                index += 1
     with open(filename, "w", encoding="utf-8") as file:
         file.writelines(lines)
