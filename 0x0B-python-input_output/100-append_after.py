056b8dc539a22852e02825b3da1870516d650d17
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-07 01:03:06 +0200

    13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Append after Module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+        for i, line in enumerate(lines):
+            if search_string in line:
+                lines.insert(i + 1, new_string)
+                i += 1
+    with open(filename, "w", encoding="utf-8") as file:
+        file.writelines(lines)
