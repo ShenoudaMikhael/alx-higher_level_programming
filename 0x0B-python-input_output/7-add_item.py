2ec029c334ab9d905f7855ad91e4b145796c420f
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 12:55:23 +0200

    fix 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,13 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
-if __name__ == "__main__":
-    a = sys.argv[1:]
-    l = []
-    if os.path.exists("add_item.json"):
+a = sys.argv[1:]
+l = []
+if os.path.exists("add_item.json"):
 
-        l = load_from_json_file("add_item.json")
-        l.extend(a)
-        save_to_json_file(l, "add_item.json")
-    else:
-        l.extend(a)
-        save_to_json_file(l, "add_item.json")
+    l = load_from_json_file("add_item.json")
+    l.extend(a)
+    save_to_json_file(l, "add_item.json")
+else:
+    l.extend(a)
+    save_to_json_file(l, "add_item.json")
