fe081acf352a077a5d52f793f89bf8fc957ec0ff
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-07 01:04:02 +0200

    13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """append after function"""
     with open(filename, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
