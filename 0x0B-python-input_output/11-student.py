4aa1bd53646e34eea7e5459c15ebc85e8956c188
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 20:54:46 +0200

    11. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Studen Class Module"""
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Init function
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return a JSON representation of the object
+        """
+        if type(attrs) == list and all(isinstance(att, str) for att in attrs):
+            return {
+                attrib: getattr(self, attrib)
+                for attrib in self.__dict__
+                if attrib in attrs
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
