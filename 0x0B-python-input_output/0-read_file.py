00168fc865cdb800e2000d9420c918e19c173f5e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 06:56:25 +0200

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """
+    Reads the contents of a file and prints it to the console.
+
+    Args:
+        filename (str, optional): The name of the file to read. Defaults to "".
+
+    Returns:
+        None
+    """
+    with open(filename, encoding="utf-8") as f:
+        for i in f.read():
+            print(i, end="")
