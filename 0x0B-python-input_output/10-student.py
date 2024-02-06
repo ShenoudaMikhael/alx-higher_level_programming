c4e1a012461ac0d19e32907cf2ef8c75b5ed01e7
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 14:48:36 +0200

    10. Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,6 +6,13 @@ class Student:
     """Student Class"""
 
     def __init__(self, first_name, last_name, age):
+        """
+        Init function
+        Args:
+            first_name (str): Student's first name
+            last_name (str): Student's last name
+            age (int): Student's age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
