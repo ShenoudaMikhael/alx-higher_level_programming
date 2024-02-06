bf6b9870997785c2b1b1af98a999047e8173f854
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-06 13:29:45 +0200

    8. Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Class to Json Module"""
-import json
 
 
 def class_to_json(obj):
@@ -11,4 +10,4 @@ def class_to_json(obj):
     Returns:
         str: The JSON string representation of the given object.
     """
-    return json.dumps(obj.__dict__)
+    return vars(obj)
