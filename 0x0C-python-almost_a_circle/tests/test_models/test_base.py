d19163256eef73ae3541bd7fe266a2ac41213c3b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-12 23:07:24 +0200

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -25,11 +25,15 @@ class TestBase(unittest.TestCase):
         self.assertEqual(obj6.id, -2)
 
     def test_to_json_string(self):
+        Base._Base__nb_objects = 0
+
         list_dicts = [{"id": 1}, {"id": 2}, {"id": 3}]
         json_string = Base.to_json_string(list_dicts)
         self.assertEqual(json_string, '[{"id": 1}, {"id": 2}, {"id": 3}]')
 
     def test_from_json_string(self):
+        Base._Base__nb_objects = 0
+
         json_string = '[{"id": 1}, {"id": 2}, {"id": 3}]'
         list_dicts = Base.from_json_string(json_string)
         self.assertEqual(len(list_dicts), 3)
@@ -38,6 +42,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(list_dicts[2], {"id": 3})
 
     def test_create(self):
+        Base._Base__nb_objects = 0
+
         dictionary = {"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}
         rectangle = Base.create(**dictionary)
         self.assertIsInstance(rectangle, Rectangle)
@@ -56,6 +62,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(square.y, 6)
 
     def test_load_from_file(self):
+        Base._Base__nb_objects = 0
+
         sqr0 = Square(id=1, size=2, x=3, y=4)
         Square.save_to_file([sqr0])
         sq1 = Square.load_from_file()
@@ -65,6 +73,22 @@ class TestBase(unittest.TestCase):
         self.assertEqual(sq1[0].size, 2)
         self.assertEqual(sq1[0].x, 3)
         self.assertEqual(sq1[0].y, 4)
+
+    def test_save_to_file(self):
+        Base._Base__nb_objects = 0
+
+        list_rects = [Rectangle(1, 2), Rectangle(3, 4), Rectangle(5, 6)]
+        Rectangle.save_to_file(list_rects)
+        with open("Rectangle.json", "r", encoding="utf-8") as file:
+            saved_rects = Base.from_json_string(file.read())
+        self.assertEqual(
+            saved_rects,
+            [
+                {"id": 1, "width": 1, "height": 2, "x": 0, "y": 0},
+                {"id": 2, "width": 3, "height": 4, "x": 0, "y": 0},
+                {"id": 3, "width": 5, "height": 6, "x": 0, "y": 0},
+            ],
+        )
 
 
 if __name__ == "__main__":
