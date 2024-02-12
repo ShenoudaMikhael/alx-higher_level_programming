466e4a2f152b6c8a4e1c3b2479f92aee564d9a98
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-12 18:30:13 +0200

    2. Area and Perimeter

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,8 +25,10 @@ class Base:
     def save_to_file(cls, list_objs):
         """Save objects to a file."""
 
-        with open("{}.json".format(cls.__name__), "w+", encoding="utf-8") as file:
-            file.write(Base.to_json_string([ob.to_dictionary() for ob in list_objs]))
+        with open("{}.json".format(cls.__name__),
+                  "w+", encoding="utf-8") as file:
+            file.write(Base.to_json_string([ob.to_dictionary()
+                                            for ob in list_objs]))
 
     @staticmethod
     def from_json_string(json_string):
