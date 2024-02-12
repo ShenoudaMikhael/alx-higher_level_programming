6e11d1f499d8032601e56f10fe17cfb470805084
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-12 22:33:40 +0200

    test1

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,8 @@
 """Base Class Unit test"""
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -22,19 +24,36 @@ class TestBase(unittest.TestCase):
         self.assertEqual(obj5.id, "A")
         self.assertEqual(obj6.id, -2)
 
-    def test_from_json_string(self):
-        Base._Base__nb_objects = 0
+    def test_to_json_string(self):
+        list_dicts = [{"id": 1}, {"id": 2}, {"id": 3}]
+        json_string = Base.to_json_string(list_dicts)
+        self.assertEqual(json_string, '[{"id": 1}, {"id": 2}, {"id": 3}]')
 
-        json_string = '[{"id": 1}]'
-        obj_list = Base.from_json_string(json_string)
-        self.assertEqual(obj_list, [{"id": 1}])
+    def test_from_json_string(self):
+        json_string = '[{"id": 1}, {"id": 2}, {"id": 3}]'
+        list_dicts = Base.from_json_string(json_string)
+        self.assertEqual(len(list_dicts), 3)
+        self.assertEqual(list_dicts[0], {"id": 1})
+        self.assertEqual(list_dicts[1], {"id": 2})
+        self.assertEqual(list_dicts[2], {"id": 3})
 
     def test_create(self):
-        Base._Base__nb_objects = 0
+        dictionary = {"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}
+        rectangle = Base.create(**dictionary)
+        self.assertIsInstance(rectangle, Rectangle)
+        self.assertEqual(rectangle.id, 1)
+        self.assertEqual(rectangle.width, 2)
+        self.assertEqual(rectangle.height, 3)
+        self.assertEqual(rectangle.x, 4)
+        self.assertEqual(rectangle.y, 5)
 
-        dictionary = {"id": 1}
-        obj = Base.create(**dictionary)
-        self.assertEqual(obj.id, 1)
+        dictionary = {"id": 2, "size": 4, "x": 5, "y": 6}
+        square = Base.create(**dictionary)
+        self.assertIsInstance(square, Square)
+        self.assertEqual(square.id, 2)
+        self.assertEqual(square.size, 4)
+        self.assertEqual(square.x, 5)
+        self.assertEqual(square.y, 6)
 
 
 if __name__ == "__main__":
