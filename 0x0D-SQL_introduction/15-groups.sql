7cdc069cf5521916f3b3c2371842be3df63d9070
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-12 23:38:10 +0200

    15. Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- select same score i
 -- cript that lists the number of records with the same score in the table second_table
-SELECT DISTINCT `score`,count(id) FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT DISTINCT `score`,count(id) as number FROM second_table GROUP BY score ORDER BY score DESC;
