47d14208aea8c7ee572455bad50b6eb68e2a32c9
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-13 22:25:55 +0200

    17. Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,11 @@
 -- lists all genres of the show NOT Dexter.
 -- script that uses the hbtn_0d_tvshows database to lists all genres of the show NOT Dexter.
-SELECT tv_genres.name
+SELECT DISTINCT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = (SELECT tv_shows.id FROM tv_shows WHERE tv_shows.title = 'Dexter');
+WHERE tv_genres.name NOT IN (SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC);
