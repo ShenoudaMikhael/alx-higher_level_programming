e73c8395a2990406cf751f3ead886add1bacb21c
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-13 23:15:10 +0200

    19. Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,6 +4,6 @@ SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
 FROM tv_shows
 JOIN tv_show_ratings on tv_shows.id = tv_show_ratings.show_id
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres on tv_show_genres.genre_id = tv_genres.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.id
 ORDER BY rating DESC;
