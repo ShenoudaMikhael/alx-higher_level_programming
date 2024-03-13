d9c9b174339b156e380e45debf63fb2cae2b2c51
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-13 19:38:54 +0200

    10. Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows JOIN tv_show_genres on tv_shows.id = tv_show_genres.genre_id
-WHERE tv_show_genres.genre_id != NULL;
+WHERE tv_show_genres.genre_id != NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
