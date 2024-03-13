d10b133c6ee237bbb0fe7fceb5d5ef23b3e9cc3a
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-13 23:27:28 +0200

    20. Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,8 @@
---script that lists all shows by rate
---script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- script that lists all shows by rate
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT tv_genres.name AS `name`, SUM(tv_show_ratings.rate) AS `rating`
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_genres.id
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
