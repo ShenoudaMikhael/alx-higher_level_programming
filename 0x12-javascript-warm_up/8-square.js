795f24497761e20f940641c1516621990f2042dd
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-08 22:51:01 +0200

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+if (!parseInt(process.argv[2])) {
+  console.log('Missing size');
+}
 let printX = '';
 for (let index2 = 0; index2 < parseInt(process.argv[2]); index2++) {
   printX += 'X';
