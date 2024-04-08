ae3406ba0a1c27efc12177a120ae4886b6126cff
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-08 23:46:10 +0200

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,18 @@
 #!/usr/bin/node
-if (!process.argv[2])
-    console.log(0);
+if (!process.argv[2]) { console.log(0); } else if (process.argv[2] && !process.argv[3]) { console.log(0); } else {
+  const args = process.argv.slice(2);
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
 
-else if (process.argv[2] && !process.argv[3])
-    console.log(0);
-else {
+  args.forEach(arg => {
+    const current = parseInt(arg);
+    if (current > largest) {
+      secondLargest = largest;
+      largest = current;
+    } else if (current > secondLargest && current !== largest) {
+      secondLargest = current;
+    }
+  });
 
-    let args = process.argv.slice(2)
-    let largest = -Infinity;
-    let secondLargest = -Infinity;
-
-    args.forEach(arg => {
-        const current = parseInt(arg);
-        if (current > largest) {
-            secondLargest = largest;
-            largest = current;
-        } else if (current > secondLargest && current !== largest) {
-            secondLargest = current;
-        }
-    });
-
-    console.log(secondLargest);
+  console.log(secondLargest);
 }
