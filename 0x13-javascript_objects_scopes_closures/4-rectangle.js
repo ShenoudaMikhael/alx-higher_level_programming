f103d503d958df8caf390bfaf643322ff3495156
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-09 18:16:55 +0200

    4. Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (!w || !h || (w <= 0 || h <= 0)) { return; }
     this.width = w;
     this.height = h;
   }
 
-  print() {
+  print () {
     let printX = '';
     for (let index2 = 0; index2 < this.width; index2++) {
       printX += 'X';
@@ -16,16 +16,15 @@ class Rectangle {
     }
   }
 
-  double() {
-    this.width *= 2
-    this.height *= 2
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 
-  rotate() {
-    let hold = this.width;
+  rotate () {
+    const hold = this.width;
     this.width = this.height;
     this.height = hold;
-
   }
 }
 module.exports = Rectangle;
