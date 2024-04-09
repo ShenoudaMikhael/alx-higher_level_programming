934225df7ddcdd254e93d6c8090aad86566ae943
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-09 17:15:36 +0200

    2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-  constructor(w, h) {
+  constructor (w, h) {
     if (!w || !h || (w <= 0 || h <= 0)) { return; }
     this.width = w;
     this.height = h;
