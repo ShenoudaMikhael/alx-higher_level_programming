20a38896ed1fd216936968e7e33c1f14719bb7cf
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-09 20:56:09 +0200

    13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,5 +2,5 @@
 const fs = require('fs');
 const a = fs.readFileSync(process.argv[2], 'utf-8');
 const b = fs.readFileSync(process.argv[3], 'utf-8');
-let c = a + b;
+const c = a + b;
 fs.writeFileSync(process.argv[4], c);
