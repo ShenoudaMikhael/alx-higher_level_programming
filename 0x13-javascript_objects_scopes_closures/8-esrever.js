666bd54fefb97a42c5c2a0808df80e93b02e4832
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-09 19:11:47 +0200

    8. Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  const q = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    const element = list[i];
+    q.push(element);
+  }
+  return q;
 };
