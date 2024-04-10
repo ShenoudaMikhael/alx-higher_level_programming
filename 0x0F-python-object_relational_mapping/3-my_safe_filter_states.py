5c1ec865f21d9f3a1b74c1f0ea8bc80457d131be
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-10 23:38:53 +0200

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     )
     cur = conn.cursor()
     cur.execute(
-        """SELECT * FROM states 
-        WHERE name LIKE BINARY %s ORDER BY id ASC""",
+        """SELECT * FROM states
+        WHERE name = %s ORDER BY id ASC""",
         (sys.argv[4],),
     )  # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
