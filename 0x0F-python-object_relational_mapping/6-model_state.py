ef88506ddff744aae9c57fe62f4cd7603b172239
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-11 18:03:31 +0200

    6. First state model

---
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link class to table in database 
+"""
+import sys
+import urllib.parse
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{username}:{password}@localhost/{database}".format(
+            username=sys.argv[1],
+            password=urllib.parse.quote_plus(sys.argv[2]),
+            database=sys.argv[3],
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+"""Model State Module"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """States Model"""
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
