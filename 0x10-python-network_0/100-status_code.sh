04ed453a9e5c2b88dc2f577c2598122c33736ee8
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-27 00:11:45 +0300

    7. Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#  7. Only status code
-# /dev/null is a file that will discard all data
+# 7. Only status code /dev/null is a file that will discard all data
 curl -o /dev/null -sw "%{http_code}" "$1"
