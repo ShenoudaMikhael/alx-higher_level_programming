fbf124395ad884ce019e71837864187061f6d324
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-26 23:15:37 +0300

    6. Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""find peak int module"""
+
+
+def find_peak(list_of_integers):
+    """find peak int"""
+    temp = None
+    for i in set(list_of_integers):
+        if not temp or i > temp:
+            temp = i
+    return temp
