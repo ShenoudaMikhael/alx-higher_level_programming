44aa4c7199909dc8b7d32d5f9fc26aecf7195731
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-26 23:56:46 +0300

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,21 +2,16 @@
 """find peak int module"""
 
 
-def div(list_of_ints, low, high):
-    """div the array and search"""
-
-    mid = int((high + low) / 2)
-    if list_of_ints[mid - 1] <= list_of_ints[mid] >= list_of_ints[mid + 1]:
-        return list_of_ints[mid]
-    elif list_of_ints[mid] > list_of_ints[mid + 1]:
-        return div(list_of_ints, low, mid - 1)
-    elif list_of_ints[mid] < list_of_ints[mid + 1]:
-        return div(list_of_ints, mid + 1, high)
-
-
 def find_peak(list_of_integers):
-    """Find peak of a list"""
-
+    """find peak int"""
     if not list_of_integers:
         return None
-    return div(list_of_integers, 0, len(list_of_integers) - 1)
+    temp = list_of_integers[len(list_of_integers) - 1]
+
+    for i in range(int(len(list_of_integers) / 2)):
+
+        tmax = max(
+            list_of_integers[i], list_of_integers[
+                len(list_of_integers) - 1 - i])
+        temp = tmax if tmax > temp else temp
+    return temp
