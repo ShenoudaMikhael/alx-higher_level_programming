c71fc9f3f9b1d16bb7e9869ad95570dd1ca0e4f1
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-27 00:31:01 +0300

    8. cURL a JSON file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  9. Catch me if you can! 
-curl -L -X PUT -d "user_id=98" -H "Origin: School" c7bd4dad0ee9.443e6dc0.alx-cod.online:5000/catch_me
+curl -L -X PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
