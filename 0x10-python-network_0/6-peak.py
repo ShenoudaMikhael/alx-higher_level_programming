1ec416de910b26477ae5d7cabf1abe835bcf492a
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-27 03:40:18 +0300

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,24 +5,20 @@
 def find_peak(list_of_integers):
     """find peak int"""
     nums = list_of_integers
-    n = len(nums)
+    array_len = len(nums)
 
-    # Edge cases
-    if n == 0:
+    if array_len == 0:
         return None
-    if n == 1:
+    if array_len == 1:
         return nums[0]
 
-    # Check if first or last element is a peak
     if nums[0] >= nums[1]:
         return nums[0]
-    if nums[n - 1] >= nums[n - 2]:
-        return nums[n - 1]
+    if nums[array_len - 1] >= nums[array_len - 2]:
+        return nums[array_len - 1]
 
-    # Check all elements except the first and last
-    for i in range(1, n - 1):
+    for i in range(1, array_len - 1):
         if nums[i] >= nums[i - 1] and nums[i] >= nums[i + 1]:
             return nums[i]
 
-    # If no peak is found
     return None
