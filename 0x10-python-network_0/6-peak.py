0f908f67043cc5484d0a10dd75d9d544a42b0c5e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-27 03:19:28 +0300

    8. cURL a JSON file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,6 @@ def find_peak(arr):
     """find peak int"""
     n = len(arr)
     for i in range(1, n - 1):
-        if arr[i] >= arr[i - 1] and arr[i] >= arr[i + 1]:
+        if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
             return arr[i]
     return None
