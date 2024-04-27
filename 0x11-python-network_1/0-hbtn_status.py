ef7b1dabd9b43919937b916a9dd3a158043ac669
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-27 15:11:50 +0300

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,4 +7,4 @@ with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
-    print("\t- utf content: {}".format(body.decode("utf-8")))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
