a03c6c15b7c0970e9d4961f4a4f1ab371454aa20
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-27 19:02:11 +0300

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode("utf-8"))
