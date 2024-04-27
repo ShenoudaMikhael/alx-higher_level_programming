04a00b45ddc3b295ed659cf5604b6f0d1994508b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-27 15:17:13 +0300

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,7 @@
 import sys
 import urllib.request
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    body = response.read()
-    print(response.headers["X-Request-Id"])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        body = response.read()
+        print(response.headers["X-Request-Id"])
