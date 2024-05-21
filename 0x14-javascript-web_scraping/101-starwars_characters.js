29e6fcf32425476c823cf056068c51e0c5efbb77
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-21 16:57:35 +0300

    7. Who was playing in this movie?

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,40 @@
 #!/usr/bin/node
 const request = require('request');
+
+const makeRequest = (element) => {
+  return new Promise((resolve, reject) => {
+    request(element, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+};
+
+const processElements = async (kk) => {
+  const qq = {};
+  for (const element of kk) {
+    qq[element.split('/')[element.split('/').length - 2]] = 0;
+    try {
+      const name = await makeRequest(element);
+      qq[element.split('/')[element.split('/').length - 2]] = name;
+      console.log(name);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return qq;
+};
+
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
     return;
   }
   const d = JSON.parse(body);
-  d.characters.forEach(element => {
-    request(element, (err, response, body) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(JSON.parse(body).name);
-    });
-  });
+  const kk = d.characters;
+
+  processElements(kk);
 });
