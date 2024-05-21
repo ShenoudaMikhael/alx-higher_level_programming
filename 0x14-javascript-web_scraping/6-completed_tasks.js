296ac3982b9f50a9d5f3c06f6e706e0607047e5c
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-21 15:41:16 +0300

    6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,17 +5,15 @@ request(process.argv[2], (err, response, body) => {
     console.log(err);
     return;
   }
-  let q = {}
-  let k = JSON.parse(body);
+  const q = {};
+  const k = JSON.parse(body);
   for (let index = 0; index < k.length; index++) {
     const element = k[index];
     if (element.completed === true) {
-      if (q.hasOwnProperty(element.userId)) {
-        q[element.userId] = q[element.userId] + 1
-
+      if (Object.prototype.hasOwnProperty.call(q, element.userId)) {
+        q[element.userId] = q[element.userId] + 1;
       } else {
-        q[element.userId] = 1
-
+        q[element.userId] = 1;
       }
     }
   }
